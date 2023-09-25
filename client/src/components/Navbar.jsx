8c6f91f627803289d85b96d2e73d4be363317e37
harsh2799 <mishraharsh27@gmail.com>
2023-09-24 23:36:54 -0400

    Added changes for Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import {
   Box,
   Flex,
   Avatar,
+  HStack,
   Text,
   Button,
   Menu,
@@ -13,18 +14,39 @@ import {
   useColorModeValue,
   Stack,
   useColorMode,
+  IconButton,
   Center,
 } from '@chakra-ui/react'
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 import { useAuth } from '../contexts/AuthContext'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+
+const NavLink = ({name, link}) => {
+  console.log(name + " - " + link);
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('#09356b'),
+      }}
+      href={link}>
+      {name}
+    </Box>
+  )
+}
 
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode()
   const { logout } = useAuth()
   const [error, setError] = useState("")
   const navigate = useNavigate()
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   async function handleLogout() {
     setError("")
@@ -38,12 +60,31 @@ export default function Navbar() {
 
     }
   }
+
+  const Links = [
+                    {link: "/Home", name: 'Home'},
+                    {link: "/Registration", name: 'Registration'},
+                    {link: "/About", name: 'About Us'},
+                ]
+
   return (
     <>
       <Box bg={"#15539e"} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box color={"white"}>Logo</Box>
-
+          <IconButton
+              size={'md'}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={'Open Menu'}
+              display={{ md: 'none' }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+          <HStack spacing={8} alignItems={'center'}>
+            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }} fontSize={"16px"} color={'white'} fontWeight={'600'}>
+              {Links.map((link) => (
+                <NavLink key={link.name} {...link}></NavLink>
+              ))}
+            </HStack>
+          </HStack>
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
               <Button onClick={toggleColorMode}>
@@ -84,6 +125,15 @@ export default function Navbar() {
             </Stack>
           </Flex>
         </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4} color={'white'}>
+              {Links.map((link) => (
+                <NavLink key={link.name} {...link}></NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
     </>
   )
--- client/src/pages/LeagueRegistration.jsx
+++ client/src/pages/LeagueRegistration.jsx
@@ -1,5 +1,75 @@
 export default function LeagueRegistration() {
     return (
-        <div>LeagueRegistration</div>
+        <Flex
+            minH={"100vh"}
+            align={"center"}
+            justify={"center"}
+            bg={useColorModeValue("gray.50", "gray.800")}
+            >
+            <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+                <Stack align={"center"}>
+                <Heading fontSize={"4xl"} textAlign={"center"}>
+                    Registration Form
+                </Heading>
+                </Stack>
+                <Box
+                rounded={"lg"}
+                bg={useColorModeValue("white", "gray.700")}
+                boxShadow={"2xl"}
+                p={8}
+                >
+                {/* {JSON.stringify(currentUser)} */}
+                {/* Error box */}
+                {error && (
+                    <Box
+                    bg="red.100" // Background color for the error box
+                    p={2} // Padding for the error box
+                    mb={2} // Margin bottom for spacing
+                    borderRadius="md" // Rounded corners
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    >
+                    <Text color="red.500" fontSize="md" fontWeight={"semibold"}>
+                        {error}
+                    </Text>
+                    </Box>
+                )}
+                <Stack spacing={4}>
+                    <FormControl id="email" isRequired>
+                    <FormLabel>Email address</FormLabel>
+                    <Input
+                        type="email"
+                        name="email"
+                        value={user.email}
+                        onChange={handleInputChange}
+                    />
+                    </FormControl>
+                    <FormControl id="password" isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                        <Input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        value={user.password}
+                        onChange={handleInputChange}
+                        />
+                        <InputRightElement h={"full"}>
+                        <Button
+                            variant={"ghost"}
+                            disabled
+                            onMouseDown={() => setShowPassword(true)}
+                            onMouseUp={() => setShowPassword(false)}
+                            onMouseLeave={() => setShowPassword(false)}
+                        >
+                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                        </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    </FormControl>
+                </Stack>
+                </Box>
+            </Stack>
+            </Flex>
     )
 }

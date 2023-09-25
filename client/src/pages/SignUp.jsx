4dc0b15349d956d7dfb1e8c65973521ff4fcae25
Harsh Shah <hr838264@dal.ca>
2023-09-24 23:35:12 -0400

    Added validations

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -17,12 +17,18 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { useAuth, googleAuthenicator, facebookAuthenticator } from "../contexts/AuthContext";
-import { FaGoogle, FaFacebook } from 'react-icons/fa';
+import {
+  useAuth,
+  googleAuthenicator,
+  facebookAuthenticator,
+} from "../contexts/AuthContext";
+import { FaGoogle, FaFacebook } from "react-icons/fa";
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const messageError = {};
+  const [passwordError, setPasswordError] = useState("");
 
   const { signup, currentUser } = useAuth(); // Ensure that useAuth() is providing the signup function
 
@@ -39,6 +45,8 @@ export default function SignUp() {
     postalCode: "",
     confirmPassword: "",
   });
+  const [validationErrors, setValidationErrors] = useState({}); // State to hold validation errors
+  const [validationMessageErrors, setValidationMessageErrors] = useState({});
 
   function handleInputChange(e) {
     const { name, value } = e.target;
@@ -46,8 +54,14 @@ export default function SignUp() {
       ...user,
       [name]: value,
     });
+
+    // Clear the validation error for this field when user starts typing again
+    setValidationErrors({
+      ...validationErrors,
+      [name]: "",
+    });
   }
-  
+
   async function handleSubmit(e) {
     e.preventDefault();
     const {
@@ -61,7 +75,22 @@ export default function SignUp() {
       postalCode,
       confirmPassword,
     } = user;
-    setError("THis is error");
+    // Validate the form before submitting
+    const errors = validateForm(user);
+    console.log(errors);
+    console.log(messageError);
+    if (
+      Object.keys(errors).length > 0 ||
+      Object.keys(messageError).length > 0
+    ) {
+      // If there are validation errors, set them and prevent form submission
+      setValidationErrors(errors);
+      setValidationMessageErrors(messageError);
+      // console.log(validationMessageErrors)
+      return;
+    }
+
+    setError(""); // Clear any previous errors
     setLoading(true);
 
     try {
@@ -76,13 +105,80 @@ export default function SignUp() {
   const handleGoogelSignin = async () => {
     console.log("Singing In with Google");
     await googleAuthenicator();
-  }
+  };
 
   const handleFacebookSignin = async () => {
     console.log("Singing in with Facebook");
     await facebookAuthenticator();
-  }
+  };
 
+  function validateForm(user) {
+    const errors = {};
+
+    // Validate firstName
+    if (!user.firstName) {
+      errors.firstName = "First name is required";
+    }
+
+    // Validate lastName
+    if (!user.lastName) {
+      errors.lastName = "Last name is required";
+    }
+
+    // Validate email
+    if (!user.email) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(user.email)) {
+      messageError.email = "Email is invalid";
+    }
+
+    // Validate password
+    if (!user.password) {
+      errors.password = "Password is required";
+    } else if (
+      !(
+        user.password.length >= 8 &&
+        /[A-Z]/.test(user.password) &&
+        /[a-z]/.test(user.password) &&
+        /\d/.test(user.password) &&
+        /[^a-zA-Z\d]/.test(user.password)
+      )
+    ) {
+      messageError.password =
+        "Password must be at least 8 characters long and contains at least one uppercase letter, one lowercase letter, one digit, and one special character ";
+      setPasswordError(
+        "Password must be at least 8 characters long and contains at least one uppercase letter, one lowercase letter, one digit, and one special character "
+      );
+    }
+
+    // Validate dateOfBirth (you can add more specific validation as needed)
+    if (!user.dateOfBirth) {
+      errors.dateOfBirth = "Date of birth is required";
+    }
+
+    // Validate phoneNumber
+    if (!user.phoneNumber) {
+      errors.phoneNumber = "Phone number is required";
+    } else if (!/^\d{10}$/.test(user.phoneNumber)) {
+      messageError.phoneNumber = "Phone number must be 10 digits";
+    }
+
+    // Validate postalCode
+    if (!user.postalCode) {
+      errors.postalCode = "Postal code is required";
+    } else if (!/^[a-zA-Z0-9]{6}$/.test(user.postalCode)) {
+      messageError.postalCode = "Postal code must be 6 characters alphanumeric";
+    }
+
+    // Validate confirmPassword
+    if (!user.confirmPassword) {
+      errors.confirmPassword = "Confirm password is required";
+    } else if (user.confirmPassword !== user.password) {
+      messageError.confirmPassword = "Passwords do not match";
+    }
+
+    return errors;
+  }
 
   return (
     <Flex
@@ -104,9 +200,10 @@ export default function SignUp() {
           boxShadow={"2xl"}
           p={8}
         >
+          {passwordError}
           {JSON.stringify(currentUser)}
           {/* Error box */}
-          {error && (
+          {Object.keys(messageError).length > 0 && (
             <Box
               bg="red.100" // Background color for the error box
               p={2} // Padding for the error box
@@ -116,9 +213,15 @@ export default function SignUp() {
               justifyContent="center"
               alignItems="center"
             >
-              <Text color="red.500" fontSize="md" fontWeight={"semibold"}>
-                {error}
-              </Text>
+              <ul>
+                {Object.keys(messageError).map((fieldName) => (
+                  <li key={fieldName}>
+                    <Text color="red.500" fontSize="md" fontWeight="semibold">
+                      {messageError[fieldName]}
+                    </Text>
+                  </li>
+                ))}
+              </ul>
             </Box>
           )}
           <Stack spacing={3}>
@@ -131,6 +234,7 @@ export default function SignUp() {
                     name="firstName"
                     value={user.firstName}
                     onChange={handleInputChange}
+                    isInvalid={validationErrors.firstName}
                   />
                 </FormControl>
               </Box>
@@ -142,6 +246,7 @@ export default function SignUp() {
                     name="lastName"
                     value={user.lastName}
                     onChange={handleInputChange}
+                    isInvalid={validationErrors.lastName}
                   />
                 </FormControl>
               </Box>
@@ -153,6 +258,7 @@ export default function SignUp() {
                 name="email"
                 value={user.email}
                 onChange={handleInputChange}
+                isInvalid={validationErrors.email}
               />
             </FormControl>
 
@@ -166,6 +272,7 @@ export default function SignUp() {
                   name="dateOfBirth"
                   value={user.dateOfBirth}
                   onChange={handleInputChange}
+                  isInvalid={validationErrors.dateOfBirth}
                 />
               </FormControl>
               {/* Add Gender */}
@@ -192,6 +299,7 @@ export default function SignUp() {
                   name="phoneNumber"
                   value={user.phoneNumber}
                   onChange={handleInputChange}
+                  isInvalid={validationErrors.phoneNumber}
                 />
               </FormControl>
               {/* Add Postal Code */}
@@ -203,6 +311,7 @@ export default function SignUp() {
                   name="postalCode"
                   value={user.postalCode}
                   onChange={handleInputChange}
+                  isInvalid={validationErrors.postalCode}
                 />
               </FormControl>
               {/* Add Phone Number */}
@@ -215,6 +324,7 @@ export default function SignUp() {
                   name="password"
                   value={user.password}
                   onChange={handleInputChange}
+                  isInvalid={validationErrors.password}
                 />
                 <InputRightElement h={"full"}>
                   <Button
@@ -239,6 +349,7 @@ export default function SignUp() {
                     name="confirmPassword"
                     value={user.confirmPassword}
                     onChange={handleInputChange}
+                    isInvalid={validationErrors.confirmPassword}
                   />
                   <InputRightElement h={"full"}>
                     <Button
@@ -280,7 +391,7 @@ export default function SignUp() {
                 size="md"
                 fontWeight="medium"
                 borderRadius="full"
-                _hover={{ bg: 'red.500', color: 'white' }}
+                _hover={{ bg: "red.500", color: "white" }}
               >
                 Sign in with Google
               </Button>
@@ -292,7 +403,7 @@ export default function SignUp() {
                 size="md"
                 fontWeight="medium"
                 borderRadius="full"
-                _hover={{ bg: 'blue.500', color: 'white' }}
+                _hover={{ bg: "blue.500", color: "white" }}
               >
                 Sign in with Facebook
               </Button>

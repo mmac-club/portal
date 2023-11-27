02a233eff5282edf13a4d5343d38381b43ee538f
harsh2799 <mishraharsh27@gmail.com>
2023-11-26 23:57:36 -0500

    Code for payment

--- server/api/controllers/payment_handler.js
+++ server/api/controllers/payment_handler.js
@@ -70,6 +70,32 @@ const createOrder = async (cart) => {
     return handleResponse(response);
   };
 
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
+const captureOrder = async (orderID) => {
+  console.log(orderID)
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderID}/capture`;
+  console.log(url)
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+  });
+
+  return handleResponse(response);
+};
+
 export const order = async (req, res, next) => {
     try {
         // use the cart information passed from the front-end to calculate the order amount detals
@@ -83,34 +109,30 @@ export const order = async (req, res, next) => {
       }
 };
 
-export const captureOrder = async (orderID) => {
-    const accessToken = await generateAccessToken();
-    const url = `${base}/v2/checkout/orders/${orderID}/capture`;
-  
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-        // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
-        // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
-        // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
-        // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
-        // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
-      },
-    });
-  
-    return handleResponse(response);
+export const capture = async (req, res, next) => {
+    try {
+
+        const orderID  = req.params.id;
+        const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
+        res.status(httpStatusCode).json(jsonResponse);
+      } catch (error) {
+        console.error("Failed to create order:", error);
+        res.status(500).json({ error: "Failed to capture order." });
+      }
   };
   
   async function handleResponse(response) {
     try {
+
       const jsonResponse = await response.json();
+      console.log(jsonResponse)
       return {
         jsonResponse,
         httpStatusCode: response.status,
       };
     } catch (err) {
+
+      console.log(err)
       const errorMessage = await response.text();
       throw new Error(errorMessage);
     }
--- server/api/routes/payment.js
+++ server/api/routes/payment.js
@@ -1,11 +1,11 @@
 import express from "express";
 
-import { order, captureOrder } from "../controllers/payment_handler.js"
+import { order, capture } from "../controllers/payment_handler.js"
 
 const router = express.Router();
 
 
 router.post("/", order)
-router.post("/:id/capture", captureOrder)
+router.post("/:id/capture", capture)
 
 export default router
